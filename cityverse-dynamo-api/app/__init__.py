6f5d4ae659bfbeaaed662d0c3aa4b78888a09041
SoufianeAmghar <soufiane35.live@gmail.com>
2023-11-13 14:48:30 +0100

    Added authentication namespace

--- cityverse-dynamo-api/app/__init__.py
+++ cityverse-dynamo-api/app/__init__.py
@@ -2,6 +2,7 @@ from flask_restx import Api
 from flask import Blueprint
 
 from .main.controller.user_controller import api as user_ns
+from .main.controller.auth_controller import api as auth_ns
 # from .main.controller.profile_controller import api as userprofile_ns
 
 
@@ -14,4 +15,5 @@ api = Api(blueprint,
           )
 
 # api.add_namespace(userprofile_ns, path='/profiles')
+api.add_namespace(auth_ns, path='/auth')
 api.add_namespace(user_ns, path='/user')
