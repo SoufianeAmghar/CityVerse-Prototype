1310ed427e0837a561639e81f4cb53f804a04672
SoufianeAmghar <soufiane35.live@gmail.com>
2024-01-18 19:25:25 +0100

    Added namespaces of product,place and events

--- cityverse-dynamo-api/app/__init__.py
+++ cityverse-dynamo-api/app/__init__.py
@@ -4,6 +4,10 @@ from flask import Blueprint
 from .main.controller.user_controller import api as user_ns
 from .main.controller.auth_controller import api as auth_ns
 from .main.controller.product_controller import api as product_ns
+from .main.controller.place_controller import api as place_ns
+from .main.controller.event_controller import api as event_ns
+from .main.controller.badge_controller import api as badge_ns
+
 
 
 blueprint = Blueprint('api', __name__)
@@ -17,3 +21,6 @@ api = Api(blueprint,
 api.add_namespace(product_ns, path='/products')
 api.add_namespace(auth_ns, path='/auth')
 api.add_namespace(user_ns, path='/user')
+api.add_namespace(place_ns, path='/place')
+api.add_namespace(event_ns, path='/event')
+api.add_namespace(badge_ns, path='/badge')
