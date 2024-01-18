1ab65f418762334aea5ae90a4db5f06b50e66fa0
SoufianeAmghar <soufiane35.live@gmail.com>
2024-01-18 19:26:12 +0100

    Document logic for video uploads and reactions count

--- cityverse-dynamo-api/app/db/dynamodb_document.py
+++ cityverse-dynamo-api/app/db/dynamodb_document.py
@@ -34,22 +34,30 @@ class Document:
 
         # Check if the file extension is in the list of allowed extensions
         return any(filename.lower().endswith(ext) for ext in allowed_extensions)
-    
-    def convert_dynamodb_item_to_string(self,item):
+
+    def is_video_file(self, filename):
+        allowed_extensions = {'.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv',
+                              '.webm', '.m4v', '.3gp', '.mpg', '.mpeg', '.rm',
+                              '.vob', '.ts', '.m2ts', '.ogv', '.asf', '.mts', '.mxf'}
+        return any(filename.lower().endswith(ext) for ext in allowed_extensions)
+
+    def convert_dynamodb_item_to_string(self, item):
         def convert_value(value):
             if isinstance(value, dict):
                 if 'S' in value:
                     return value['S']
                 elif 'BOOL' in value:
                     return value['BOOL']
+                elif 'N' in value:
+                    return value['N']
                 elif 'L' in value:
                     return [convert_value(item) for item in value['L']]
                 elif 'M' in value:
-                     return {key: convert_value(val) for key, val in value['M'].items()}
+                    return {key: convert_value(val) for key, val in value['M'].items()}
             elif isinstance(value, list):
                 return [convert_value(item) for item in value]
             else:
-                 return str(value)
+                return str(value)
 
         result = {}
         for key, value in item.items():
@@ -67,7 +75,7 @@ class Document:
 
     def save(self, item):
         dynamodb_resource = boto3.resource('dynamodb')
-        table = dynamodb_resource.Table(self.__TABLE_NAME__)      
+        table = dynamodb_resource.Table(self.__TABLE_NAME__)
         table.put_item(Item=item)
 
     def load(self, dynamodb_client=None, query=None):
@@ -78,9 +86,9 @@ class Document:
             query = {'id': {'S': self._id}}
 
         response = dynamodb_client.get_item(
-                TableName=self.__TABLE_NAME__,
-                Key=query  
-            )
+            TableName=self.__TABLE_NAME__,
+            Key=query
+        )
 
         item = response.get('Item')
         if item:
@@ -100,14 +108,14 @@ class Document:
 
             item = response.get('Item')
             if item:
-                return item
+                return self.convert_dynamodb_item_to_string(item)
             else:
                 return None
         except Exception as e:
             # Handle any exceptions or errors here
             logging.error(f"Error: {e}")
             return None
-    
+
     def query(self, index, condition, value):
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(self.__TABLE_NAME__)
@@ -122,12 +130,9 @@ class Document:
             ExpressionAttributeValues=value
         )
 
-        
-
         items = response.get('Items', [])
 
         return items if items else None
-
 
     def delete(self, dynamodb_client=None):
         if not dynamodb_client:
@@ -144,16 +149,17 @@ class Document:
         return {}
 
     def from_dict(self, d):
-     if d:
-        for key, value in d.items():
-            if key == 'password':
-                # Handle password separately
-                self.password_hash = bcrypt.hashpw(str(value).encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-            else:
-                setattr(self, key, value)
-     else:
-        self._id = None
-     return self
+        if d:
+            for key, value in d.items():
+                if key == 'password':
+                    # Handle password separately
+                    self.password_hash = bcrypt.hashpw(str(value).encode(
+                        'utf-8'), bcrypt.gensalt()).decode('utf-8')
+                else:
+                    setattr(self, key, value)
+        else:
+            self._id = None
+        return self
 
     def get_all(self, dynamodb_client=None):
         if not dynamodb_client:
@@ -173,26 +179,83 @@ class Document:
         table = dynamodb_client.Table(cls.__TABLE_NAME__)
         response = table.delete()
         return response
-    
 
-    def upload_profile_image_to_s3(self,image_file):
-      s3 = boto3.client('s3')
+    def upload_image_to_s3(self, image_file):
+        s3 = boto3.client('s3')
 
-      image_key = f"{self.__S3_OBJECT_PREFIX__}{generate_id()}"
+        image_key = f"{self.__S3_OBJECT_PREFIX__}{generate_id()}"
 
-      file_extension = Path(image_file.filename).suffix.lower()
-      if not file_extension or not self.is_image_file(file_extension):
+        file_extension = Path(image_file.filename).suffix.lower()
+        if not file_extension or not self.is_image_file(file_extension):
             logging.error("Invalid image file format.")
             return None
 
-      try:
-          s3.upload_fileobj(image_file, self.__BUCKET_NAME__, image_key, ExtraArgs={'ContentType': f'image/{file_extension[1:]}'})
-          profile_image_url = f"https://{self.__BUCKET_NAME__}.s3.amazonaws.com/{image_key}"
-          logging.info(f"Image uploaded to S3 successfully. URL: {profile_image_url}")
-          return profile_image_url
-      except FileNotFoundError:
-          logging.error("Profile image file not found.")
-          return None
-      except NoCredentialsError:
-          logging.error("Credentials not available for uploading profile image to S3.")
-          return None
+        try:
+            s3.upload_fileobj(image_file, self.__BUCKET_NAME__, image_key, ExtraArgs={
+                              'ContentType': f'image/{file_extension[1:]}'})
+            profile_image_url = f"https://{self.__BUCKET_NAME__}.s3.amazonaws.com/{image_key}"
+            logging.info(
+                f"Image uploaded to S3 successfully. URL: {profile_image_url}")
+            return profile_image_url
+        except FileNotFoundError:
+            logging.error("Profile image file not found.")
+            return None
+        except NoCredentialsError:
+            logging.error(
+                "Credentials not available for uploading profile image to S3.")
+            return None
+
+    def upload_video_to_s3(self, video_file):
+        s3 = boto3.client('s3')
+
+        video_key = f"{self.__S3_OBJECT_PREFIX__}{self.generate_id()}"
+
+        file_extension = Path(video_file.filename).suffix.lower()
+        if not file_extension or not self.is_video_file(file_extension):
+            logging.error("Invalid video file format.")
+            return None
+
+        try:
+            s3.upload_fileobj(video_file, self.__BUCKET_NAME__, video_key, ExtraArgs={
+                              'ContentType': f'video/{file_extension[1:]}'})
+            video_url = f"https://{self.__BUCKET_NAME__}.s3.amazonaws.com/{video_key}"
+            logging.info(
+                f"Video uploaded to S3 successfully. URL: {video_url}")
+            return video_url
+        except FileNotFoundError:
+            logging.error("Video file not found.")
+            return None
+        except NoCredentialsError:
+            logging.error(
+                "Credentials not available for uploading video to S3.")
+            return None
+
+    def save_incremental(self, post_id, data):
+        dynamodb_resource = boto3.resource('dynamodb')
+        table = dynamodb_resource.Table(self.__TABLE_NAME__)
+        reaction = data['reaction']
+        table.update_item(
+            Key={'id': post_id},
+            UpdateExpression=f'SET reactions.#reaction = reactions.#reaction + :incr',
+            ExpressionAttributeNames={
+                '#reaction': reaction,
+            },
+            ExpressionAttributeValues={
+                ':incr': 1,
+            },
+        )
+
+    def save_decremental(self, post_id, data):
+        dynamodb_resource = boto3.resource('dynamodb')
+        table = dynamodb_resource.Table(self.__TABLE_NAME__)
+        reaction = data['reaction']
+        table.update_item(
+            Key={'id': post_id},
+            UpdateExpression=f'SET reactions.#reaction = reactions.#reaction - :decr',
+            ExpressionAttributeNames={
+                '#reaction': reaction,
+            },
+            ExpressionAttributeValues={
+                ':decr': 1,
+            },
+        )
