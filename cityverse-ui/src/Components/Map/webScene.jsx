7245a1b26813651be6f6f15229b0f4fca14dd701
abdeldaoudi <abdel.daoudiensa@gmail.com>
2024-03-06 17:20:48 +0100

    fixing

--- cityverse-ui/src/Components/Map/webScene.jsx
+++ cityverse-ui/src/Components/Map/webScene.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const UnityScene = () => {
-  // const gitlabPagesURL = 'https://sustainabledevelopmentvr-guillaumecailhe-05f65f55681da3657133a5.gitlab.io/cfdc2f82-8b16-4c96-b4ec-895eba985914'; // Replace with your GitLab Pages URL
+  // const gitlabPagesURL = 'https://141.94.19.177/3d_map/index.html'; // Replace with your GitLab Pages URL
   const gitlabPagesURL = `${process.env.PUBLIC_URL}/3d_map/index.html`;
   return (
     <iframe
       title="unity-webgl"
       src={gitlabPagesURL}
       width="100%"
-      height="100vh"
+      height="650px"
       frameBorder="0"
       scrolling="no"
     ></iframe>
